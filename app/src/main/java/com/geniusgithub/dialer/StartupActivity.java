088f17b329d29037df13e262d1507b184d81f5ea
geniusgithub <lance.wyj@gmail.com>
2016-12-28 20:59:27 +0800

    add startup activity

---
+++ app/src/main/java/com/geniusgithub/dialer/StartupActivity.java
@@ -0,0 +1,14 @@
+package com.geniusgithub.dialer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.startup_layout);
+    }
+}
