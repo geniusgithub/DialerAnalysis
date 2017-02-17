765d2b072146e663396dbd429c390167474ff419
geniusgithub <lance.wyj@gmail.com>
2017-02-17 19:29:19 +0800

    add main subinfo

--- app/src/main/java/com/geniusgithub/dialer/sim/SimInfoFragment.java
+++ app/src/main/java/com/geniusgithub/dialer/sim/SimInfoFragment.java
@@ -6,10 +6,15 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.telecom.PhoneAccount;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +31,7 @@ public class SimInfoFragment extends Fragment implements View.OnClickListener{
     private View mRootView;
     private TextView mTvSub;
     private Button mBtnSub;
-
+    private TextView mTvMainCard;
 
     @Override
     public void onAttach(Activity activity) {
@@ -48,18 +53,53 @@ public class SimInfoFragment extends Fragment implements View.OnClickListener{
         mTvSub = (TextView) view.findViewById(R.id.tv_sub);
         mBtnSub = (Button) view.findViewById(R.id.btn_sub);
         mBtnSub.setOnClickListener(this);
+        mTvMainCard = (TextView) view.findViewById(R.id.tv_mainCard);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_sub:
-                getSub();
+                updateMainCard();
+                updateSub();
                 break;
         }
     }
 
-    private void getSub(){
+    private void updateMainCard(){
+        TelecomManager telecomManager = getTelecomManager(mContext);
+        PhoneAccountHandle phoneAccountHandle = telecomManager.getUserSelectedOutgoingPhoneAccount();
+        int mainSubID = -1;
+        if (phoneAccountHandle != null){
+            PhoneAccount phoneAccount =  telecomManager.getPhoneAccount(phoneAccountHandle);
+            TelephonyManager telephonyManager = getTelephonyManager(mContext);
+            mainSubID = telephonyManager.getSubIdForPhoneAccount(phoneAccount);
+            Log.d(TAG, "telephonyManager.getSubIdForPhoneAccount  = " + mainSubID);
+        }else{
+            Log.e(TAG, "getUserSelectedOutgoingPhoneAccount = null");
+        }
+
+        final SubscriptionInfo subInfo = SubscriptionManager.from(mContext).getActiveSubscriptionInfo(mainSubID);
+        String mainSubInfo = "null";
+        if (subInfo != null){
+            mainSubInfo = getSimpleSubInfo(subInfo);
+        }else{
+            Log.e(TAG, "getActiveSubscriptionInfo = null");
+        }
+
+        int globalSubID = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.MULTI_SIM_VOICE_CALL_SUBSCRIPTION,
+                SubscriptionManager.INVALID_SUBSCRIPTION_ID);
+
+        int globalPrompt = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.MULTI_SIM_VOICE_PROMPT, -1);
+
+        String value = "globalSubID = " + globalSubID + "\nglobalPrompt = " + globalPrompt
+                + "\nmainSubID = " + mainSubID + "\nmainSubInfo -->\n" + mainSubInfo + "\n======================\n";
+        mTvMainCard.setText(value);
+    }
+
+    private void updateSub(){
         SubscriptionManager subscriptionManager = SubscriptionManager.from(mContext);
 
         int sub1[] = SubscriptionManager.getSubId(0);
@@ -106,9 +146,30 @@ public class SimInfoFragment extends Fragment implements View.OnClickListener{
 
     }
 
+    public String getSimpleSubInfo(SubscriptionInfo info) {
+        if (info == null){
+            return "null";
+        }
+
+        StringBuffer sBuffer = new StringBuffer();
+        sBuffer.append("getDisplayName = " + info.getDisplayName() +
+                "\ngetIccId = " + info.getIccId() +
+                "\ngetNumber = " + info.getNumber() +
+                "\ngetSimSlotIndex = " + info.getSimSlotIndex() +
+                "\ngetSubscriptionId = " + info.getSubscriptionId());
+
+        boolean isVolteEnable = LoadMethod.TelephonyManager_isVolteAvailableUsingSubId(getTelephonyManager(mContext), info.getSubscriptionId());
+        sBuffer.append("\nisVolteAvailableUsingSubId = " + isVolteEnable);
+
+
+        return sBuffer.toString();
+    }
+
     private static TelephonyManager getTelephonyManager(Context context) {
         return (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
-
+    private static TelecomManager getTelecomManager(Context context) {
+        return (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE);
+    }
 }
